5d3960b7135f8d8fd8e00c03d7c2fba0902a03e8
jtrznadel <trznadeljakub3ai@gmail.com>
2024-07-30 14:24:46 +0200

    refactor and update on_boarding code

--- lib/features/onboarding/presentation/views/onboarding_screen.dart
+++ lib/features/onboarding/presentation/views/onboarding_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peak_mates/core/common/widgets/image_background.dart';
+import 'package:peak_mates/core/extensions/context_extension.dart';
 import 'package:peak_mates/core/res/colors.dart';
 import 'package:peak_mates/core/res/media_res.dart';
 import 'package:peak_mates/core/res/string_res.dart';
@@ -17,68 +19,68 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   bool _isFinished = false;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      fit: StackFit.expand,
-      children: [
-        ColorFiltered(
-          colorFilter: const ColorFilter.mode(
-            Colors.grey,
-            BlendMode.saturation,
-          ),
-          child: Image.asset(
-            MediaRes.onboardingBackground,
-            fit: BoxFit.cover,
-          ),
-        ),
-        SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(30),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  StringRes.onBoardingTitle,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                Text(
-                  StringRes.appName,
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                Text(
-                  StringRes.onBoardingSubtitle,
-                  style: Theme.of(context).textTheme.bodyLarge,
-                ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Center(
-                  child: SwipeableButtonView(
-                    onFinish: () {
-                      Navigator.of(context).pushNamed('/selection');
-                    },
-                    isFinished: _isFinished,
-                    onWaitingProcess: () {
-                      Future.delayed(const Duration(seconds: 1), () {
-                        setState(() {
-                          _isFinished = true;
-                        });
-                      });
-                    },
-                    activeColor: AppColors.grayDarkColor.withOpacity(.6),
-                    buttonColor: AppColors.primaryColor,
-                    buttonWidget: const Icon(
-                      Icons.landscape,
-                    ),
-                    buttonText: "Explore now",
-                    buttontextstyle: Theme.of(context).textTheme.bodyLarge,
-                  ),
-                )
-              ],
+    return ImageBackground(
+      image: MediaRes.onboardingBackground,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            StringRes.onBoardingInfo,
+            style: context.theme.textTheme.bodyMedium!.copyWith(
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.italic,
+              color: AppColors.lightTextColor,
             ),
+            textAlign: TextAlign.center,
           ),
-        )
-      ],
+          const Spacer(),
+          Text(
+            StringRes.onBoardingTitle,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  color: AppColors.lightTextColor,
+                ),
+          ),
+          Text(
+            StringRes.appName,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          Text(
+            StringRes.onBoardingSubtitle,
+            style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontSize: 24,
+                  color: AppColors.lightTextColor,
+                ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: SwipeableButtonView(
+              onFinish: () {
+                Navigator.of(context).pushNamed('/selection');
+              },
+              isFinished: _isFinished,
+              onWaitingProcess: () {
+                Future.delayed(const Duration(seconds: 1), () {
+                  setState(() {
+                    _isFinished = true;
+                  });
+                });
+              },
+              activeColor: AppColors.grayDarkColor.withOpacity(.6),
+              buttonColor: AppColors.primaryColor,
+              buttonWidget: const Icon(
+                Icons.arrow_forward_ios,
+              ),
+              buttonText: "Explore now",
+              buttontextstyle: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: AppColors.lightTextColor,
+                  ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
